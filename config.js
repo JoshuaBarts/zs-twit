c7e6de52f7fa24799118135fba309a381f1303ac
JoshuaBarts <80853126+JoshuaBarts@users.noreply.github.com>
2021-10-17 20:20:57 -0700

    put tokens here

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         'FZmO6FpTpVi9Gd4NH4NKc7BMC',
-    consumer_secret:      'LIHSFEhpCCIUVRTwjSaaUSSgsGtYhhQVz7HXWonL0VNUnkQT3J',
-    access_token:         '1164418397741834240-Kdu6C7aI59YCbCGSGqc0XofZ3azEDK',
-    access_token_secret:  'rmtc59A3GGqRApUkolc2Jbj3ZXtplYV6sni6NLME2cchh'
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...'
 }
